f126811221823fa932f6828bb50c41fb398ece8e
solutionman <solutionman@inbox.ru>
2022-01-21 09:46:55 +0700

    post request to save customer

--- customer/src/main/java/com/services/customer/CustomerService.java
+++ customer/src/main/java/com/services/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.services.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -12,6 +12,6 @@ public record CustomerService() {
                 .build();
         // todo: check if email valid
         // todo: check if email not taken
-        // todo: store customer in db
+        customerRepository.save(customer);
     }
 }
--- readme.md
+++ readme.md
@@ -10,9 +10,25 @@ sudo docker ps -a
 http://localhost:5050  
 password: password
 
-
 in IDEA  
 jdbc:postgresql://localhost:5432/postgres  
 user:lom  
 pass:password  
 
+in PGadmin create new database "customer"
+![](pics/customerDB.png)
+
+run CustomerApplication with IDEA
+
+open Postman, send POST request
+url: localhost:8080/api/v1/customers
+Body: raw JSON
+
+{
+"firstName":"Mister",
+"lastName":"Twister",
+"email":"mistertwister@mail.com"
+}
+![](pics/PostmanSendCustomer.png)
+
+SELECT * FROM customer;
